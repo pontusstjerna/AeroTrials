ed0f4516cef62fb73da40259af45676444439496
Pontus Stjernström <pontus@stjernstrom.nu>
2019-02-13 20:12:26 +0100

    Revert to use throttle control to not be too easy :)

--- src/game/Aeroplane.java
+++ src/game/Aeroplane.java
@@ -18,9 +18,9 @@ public class Aeroplane {
     private final int MAX_FORCE = 150; // newtons
     private final double MAX_ACC = 3;
     private final double MAX_DEACC = 1;
-    private final double LIFT_FACTOR = 2.5;//0.017;
+    private final double LIFT_FACTOR = 3;//0.017;
     private final double GRAVITY = 9.81 * 10;
-    private final double ANGULAR_DRAG = 1.2;
+    private final double ANGULAR_DRAG = 1;
     private final int MIN_ENGINE_START_SPEED = 2;
     private final int MIN_CRASH_VELOCITY = 20;
 
@@ -32,8 +32,8 @@ public class Aeroplane {
     private Vector velocity; // m/s
     private double rotation = 0;
     private double torque = 0; // rads / sec
-    private double throttle = 1; // between 0 and 1
-    private boolean accelerating = true;
+    private double throttle; // between 0 and 1
+    private boolean accelerating = false;
     private boolean engineRunning = true;
     private Crash crash = null;
     private SmokeSystem smokeSystem;
@@ -92,7 +92,7 @@ public class Aeroplane {
     }
 
     public void release() {
-        accelerating = true;
+        accelerating = false;
     }
 
     public double getX() {
@@ -140,7 +140,7 @@ public class Aeroplane {
     }
 
     private void adjustSpeed(double dTime) {
-        if (true) {
+        if (accelerating) {
             if (throttle < 1) {
                 throttle += MAX_ACC * dTime;
             } else {
@@ -223,7 +223,7 @@ public class Aeroplane {
 
     private void applyDrag(double dTime) {
         // From: https://en.wikipedia.org/wiki/Drag_(physics)
-        final double p = 0.5;
+        final double p = 1.2;
         final double v = velocity.getLength();
         final double C_D = 0.04;
         final double A = 3.14; // Propeller area
